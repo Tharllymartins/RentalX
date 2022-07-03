791399d7f554511b5c8a913861809f79b63be5ab
tharlly <tharlly@pop-os.localdomain>
2022-07-03 17:31:10 -0300

    created categories route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
+app.use(express.json());
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => {
     console.log("Server is running at http://localhost:3333")
---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+
+
+const categoriesRoutes = Router();
+
+const categories = [];
+
+categoriesRoutes.post("/", (req, res) => {
+    const { name, description } = req.body;
+
+    categories.push({
+        name,
+        description
+    })
+
+    return res.status(201).send()
+})
+
+
+export { categoriesRoutes };
+
